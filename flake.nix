89c79b869ec9ccce56e0246dc671798d24c57db0
lovedeepsingh-07 <lovedeepsinghbrar07@gmail.com>
2025-11-24 16:30:14 +0530

    feat: build raylib+fmt c++ project with cross-compilation by using justfiles only

--- flake.nix
+++ flake.nix
@@ -11,14 +11,29 @@
   outputs = { ... }@inputs:
     inputs.flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import inputs.nixpkgs { inherit system; overlays = [(import inputs.rust-overlay)]; };
-        rust-pkg = pkgs.rust-bin.stable."1.88.0".default;
+        pkgs = import inputs.nixpkgs { inherit system; overlays = []; };
+        cross-pkgs = pkgs.pkgsCross.mingwW64;
+        rust-bin = inputs.rust-overlay.lib.mkRustBin { }
+          pkgs.pkgsCross.mingwW64.buildPackages;
+        rust-pkg = rust-bin.stable."1.88.0".default.override {
+          extensions = [ "rust-src" ];
+          targets = [ "x86_64-pc-windows-gnu" ];
+        };
+        deps = import ./deps.nix { inherit pkgs; };
       in
       {
         formatter = pkgs.nixfmt-classic;
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = [ pkgs.cmake rust-pkg pkgs.pkg-config pkgs.bear ];
+        devShells.default = cross-pkgs.mkShell {
+          nativeBuildInputs = [ pkgs.cmake rust-pkg pkgs.pkg-config pkgs.bear];
+          buildInputs = [ cross-pkgs.windows.pthreads ];
 		  shellHook = ''zsh'';
+        };
+        apps.setup = inputs.flake-utils.lib.mkApp {
+          drv = pkgs.writeShellApplication {
+            name = "setup";
+            runtimeInputs = [ ];
+            text = deps.setup_script;
+          };
         };
       });
 }
--- justfile
+++ justfile
@@ -1,8 +1,36 @@
+BINARY_NAME := "basement"
+TOOLCHAIN_FILE := env_var('PWD') + "/mingw-w64-toolchain.cmake"
+
 default:
 	@just -l
 
-build:
-	@cargo build
+setup:
+	@nix run .#setup
 
-run:
-	@cargo run
+build-fmt:
+	@mkdir -p build/fmt/install
+	@cmake -S deps/fmt -B build/fmt \
+		-DCMAKE_TOOLCHAIN_FILE={{TOOLCHAIN_FILE}} \
+		-DFMT_TEST=OFF \
+		-DCMAKE_INSTALL_PREFIX=build/fmt/install
+	@cmake --build ./build/fmt
+	@cmake --install ./build/fmt
+
+build-raylib:
+	@mkdir -p build/raylib/install
+	@cmake -S deps/raylib -B build/raylib \
+		-DCMAKE_TOOLCHAIN_FILE={{TOOLCHAIN_FILE}} \
+		-DBUILD_EXAMPLES=OFF \
+		-DCMAKE_INSTALL_PREFIX=build/raylib/install
+	@cmake --build ./build/raylib
+	@cmake --install ./build/raylib
+
+build: build-fmt build-raylib
+	@x86_64-w64-mingw32-g++ ./src/main.cpp  \
+		-Ibuild/raylib/install/include -Lbuild/raylib/install/lib -lraylib \
+		-lgdi32 -lwinmm \
+		-Ibuild/fmt/install/include -Lbuild/fmt/install/lib -lfmt \
+		-o build/{{BINARY_NAME}}
+
+run: build
+	@./build/basement.exe
