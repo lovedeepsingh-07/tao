7613d86aa86ec0f1485ab0c0773e9da5ca68b753
lovedeepsingh-07 <lovedeepsinghbrar07@gmail.com>
2025-12-05 11:36:13 +0530

    feat: child-process output streaming and comments

--- .build/tao.rs
+++ .build/tao.rs
@@ -1,9 +1,12 @@
 use std::{
     collections::HashMap,
     sync::{Mutex, OnceLock},
+    io::BufRead,
 };
 
-// ------ global state ------
+// ------------------------------
+// -------- global state --------
+// ------------------------------
 #[derive(Debug)]
 struct TaoState {
     cmd_list: HashMap<String, CmdCallback>,
@@ -34,8 +37,8 @@ pub fn end() -> Result<(), String> {
     let mut mutex_gaurd = TAO_STATE.get().unwrap().lock().unwrap();
     let TaoState { cmd_list, cmd_args } = &mut *mutex_gaurd;
 
-    println!("[DEBUG] {:#?}", cmd_list);
-    println!("[DEBUG] {:#?}", cmd_args);
+    debug::debug(&format!("{:#?}", cmd_list));
+    debug::debug(&format!("{:#?}", cmd_args));
 
     if let Some(cmd_arg) = cmd_args.get(1){
         if let Some(run_cmd) = cmd_list.get(cmd_arg) {
@@ -46,7 +49,31 @@ pub fn end() -> Result<(), String> {
     Ok(())
 }
 
-// ------ commands ------
+// -----------------------
+// -------- debug --------
+// -----------------------
+pub mod debug {
+    pub fn debug(input: &str) {
+        let flag_str = "\x1b[90m[DEBUG]\x1b[0m";
+        println!("{} {}", flag_str, input);
+    }
+    pub fn info(input: &str) {
+        let flag_str = "\x1b[94m[INFO]\x1b[0m";
+        println!("{} {}", flag_str, input);
+    }
+    pub fn warn(input: &str) {
+        let flag_str = "\x1b[93m[WARN]\x1b[0m";
+        println!("{} {}", flag_str, input);
+    }
+    pub fn error(input: &str) {
+        let flag_str = "\x1b[91m[ERROR]\x1b[0m";
+        println!("{} {}", flag_str, input);
+    }
+}
+
+// ----------------------------
+// -------- commands ----------
+// ----------------------------
 #[allow(non_camel_case_types)]
 pub type CmdCallback_Signature = Box<dyn Fn() + Send + Sync>;
 pub struct CmdCallback(pub CmdCallback_Signature);
@@ -64,8 +91,9 @@ pub fn create_cmd(name: &str, callback: CmdCallback_Signature) {
     cmd_list.insert(String::from(name), CmdCallback(callback));
 }
 
-
-// ------ executables ------
+// -----------------------------
+// -------- executables --------
+// -----------------------------
 #[derive(Debug)]
 pub struct ExecutableConfig {
     pub cc: String,
@@ -83,10 +111,30 @@ pub fn create_executable(config: ExecutableConfig) -> Result<Executable, String>
     cmd.arg(&config.source_file);
     cmd.arg("-o")
         .arg(format!("{}/{}", &config.build_dir, &config.name));
+
+    // NOTE: just testing this thing, maybe it should not be here, but who knows, the one thing
+    // that is certain is that this should either be here or in the `tao::install` function
+    cmd.stdout(std::process::Stdio::piped());
+
     Ok(Executable { cmd, config })
 }
 pub fn install(exec: &mut Executable) -> Result<(), String> {
-    let _ = exec.cmd.output().map_err(|e| e.to_string());
     let _ = exec.config;
+
+    // NOTE: testing this code out
+    let mut child_process = exec.cmd.spawn().map_err(|e| e.to_string())?;
+    if let Some(output) = child_process.stdout.take() {
+        let mut reader = std::io::BufReader::new(output);
+        for line in reader.lines(){
+            let line = match line {
+                Ok(out) => out,
+                Err(e) => {
+                    return Err(e.to_string());
+                }
+            };
+            debug::debug(&line);
+        }
+    }
+
     Ok(())
 }
